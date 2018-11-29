171c154140f9490475b656bcd03cfaeb219f9ea9
Joseph Aguilar <josephraguilar@gmail.com>
2018-11-28 22:47:37 -0800

    fin

--- lab3/src/app/app.module.ts
+++ lab3/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainHeaderComponent,
+    MainContentComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
